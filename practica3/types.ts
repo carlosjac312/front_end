e0ab2207438de4193204d2a2ede946bba9dc1f55
carlosjac312 <115487243+carlosjac312@users.noreply.github.com>
2024-03-18 00:35:19 +0100

    Add files via upload

---
+++ practica3/types.ts
@@ -0,0 +1,10 @@
+export type jobOffer = {
+    slug:string;
+    company_name:string;
+    title:string;
+    description:string;
+    remote:boolean;
+    url:string;
+    location:string;
+    created_at:number;
+}
